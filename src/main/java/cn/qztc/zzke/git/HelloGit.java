9c3ee77914fad8dc29006ad8d4ed2ff89cecd762
zzke599 <956468746@qq.com>
2021-08-24 20:19:33 +0800

    third commit

--- src/main/java/cn/qztc/zzke/git/HelloGit.java
+++ src/main/java/cn/qztc/zzke/git/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
         int a = 18, b = 17;
         System.out.println(String.format("%d + %d = %d", a, b, sum(a, b)));
     }
