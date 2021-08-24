11305dee065077dfd77de033fc33f7966790a39c
zzke599 <956468746@qq.com>
2021-08-24 20:07:04 +0800

    add method sum

--- src/main/java/cn/qztc/zzke/git/HelloGit.java
+++ src/main/java/cn/qztc/zzke/git/HelloGit.java
@@ -3,5 +3,10 @@ package cn.qztc.zzke.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        int a = 18, b = 17;
+        System.out.println(String.format("%d + %d = %d", a, b, sum(a, b)));
+    }
+    public static int sum(int a,int b) {
+        return  a + b;
     }
 }
